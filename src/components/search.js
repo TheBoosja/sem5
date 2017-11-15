f63a5aadd3765b7286c7fe57677a01d9221c8c08
unknown <mathias.iversen487@gmail.com>
2017-11-15 14:16:12 +0100

    init

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import '../css/content.css';
 import '../css/search.css';
+import React, { Component } from 'react';
 
 class Search extends Component {
 	render() {
 		return (
-			<div>
+			<div className='content'>
 				<h3>Search bar:</h3>
 				<input className='search' type='text' placeholder='Search' />
 			</div>
