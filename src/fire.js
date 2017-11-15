119985a4fa3c7722584c21e16f7decd7a84c7f11
unknown <mathias.iversen487@gmail.com>
2017-11-15 14:12:03 +0100

    init

--- src/fire.js
+++ src/fire.js
@@ -1,13 +1,13 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDUysToslMYSrFNQ8FWiXdPKPbvBVijXgA", 
-	authDomain: "sem5-4199d.firebaseapp.com",
-	databaseURL: "https://sem5-4199d.firebaseio.com",
-	projectId: "sem5-4199d",
-	storageBucket: "sem5-4199d.appspot.com", 
-	messagingSenderId: "765890159974"
+	apiKey: 'AIzaSyDUysToslMYSrFNQ8FWiXdPKPbvBVijXgA', 
+	authDomain: 'sem5-4199d.firebaseapp.com',
+	databaseURL: 'https://sem5-4199d.firebaseio.com',
+	projectId: 'sem5-4199d',
+	storageBucket: 'sem5-4199d.appspot.com', 
+	messagingSenderId: '765890159974'
 };
 var fire = firebase.initializeApp(config);
 
