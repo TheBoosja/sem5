bd828eea7f1aac19ac065b496cd813628e1c5d86
unknown <mathias.iversen487@gmail.com>
2017-11-15 14:14:46 +0100

    Powered-by icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,19 @@
+import '../css/footer.css';
 import React, { Component } from 'react';
 import tmdb from '../TMDB.svg';
+import logo from '../logo.svg';
+import { title } from '../info';
 
 class Footer extends Component {
 	render() {
 		return (
-			<footer>
-				<img height={100} src={tmdb} alt='tmdb' />
-			</footer>
+			<div className='footer'>
+				<footer>
+					<small>{title}</small>
+					<a href='https://reactjs.net/' className='logo'><img id='react' src={logo} alt='react' /></a>
+					<a href='https://www.tmdb.org'><img id='tmdb' src={tmdb} alt='tmdb' /></a>
+				</footer>
+			</div>
 		);
 	}
 }
