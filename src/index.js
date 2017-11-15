5736413b97d2e2b0dea9e553c0c857793622ea12
unknown <mathias.iversen487@gmail.com>
2017-11-15 14:13:57 +0100

    Configured routes & middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+import './css/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import registerServiceWorker from './registerServiceWorker';
+// Middleware
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import App from './components/App';
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const createStoreWithMiddleware = applyMiddleware(middleware, reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<App store={store} />
+		</ConnectedRouter>
+	</Provider>
+	, document.getElementById('root')
+);
 registerServiceWorker();
