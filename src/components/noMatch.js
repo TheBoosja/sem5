13fb146d3a2dc19e6f8205cbfa4f04ae735d34e0
unknown <mathias.iversen487@gmail.com>
2017-11-17 08:53:42 +0100

    Routing handling

---
+++ src/components/noMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class NoMatch extends Component {
+	render() {
+		return (
+			<div className='content'>
+				<h2>404</h2>
+				<h4>Doesn't look like anything to me.</h4>
+				<h5> - Westworld</h5>
+			</div>
+		);
+	}
+}
