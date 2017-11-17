5d58c449b2faa25a4f7d73b090fc0677bd85ae13
unknown <mathias.iversen487@gmail.com>
2017-11-17 08:58:13 +0100

    Name changed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 
-import auth from './reducer_auth';
+import auth from './reducerAuth';
 
 const rootReducer = combineReducers({
 	router,
