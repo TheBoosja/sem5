d4b41267c0a3f29c4ac19830cad355d31062f80d
unknown <mathias.iversen487@gmail.com>
2017-11-30 12:55:37 +0100

    fetch to axios

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { SEARCH_RESULT, EMPTY_RESULT } from './types';
 
 const ROOT_URL = 'https://api.themoviedb.org/3/search/tv?api_key=e88fbcbf7600bbb2fbc4469a5d75ca09&language=en-US';
@@ -6,18 +7,13 @@ export function search(query) {
 	return (dispatch) => {
 		const url = `${ROOT_URL}&query=${query}&page=1`;
 
-		fetch(url).then(res => {
-			if (res.ok) {
-				return res.json();
-			}
-			else {
-				console.log(res.status, res.statusText);
-			}
-		}).then(res => {
+		axios(url).then(res => {
 			dispatch({
 				type: SEARCH_RESULT,
-				payload: res
+				payload: res.data
 			});
+		}).catch(error => {
+			console.log(error);
 		});
 	};
 }
