570845b0e68391ac0e7dee266e2abf5e66e87f4f
unknown <mathias.iversen487@gmail.com>
2017-11-30 12:54:01 +0100

    Date format

--- src/components/search/searchShowItem.js
+++ src/components/search/searchShowItem.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 import NoPoster from '../../img/no_poster_w154.png';
 
 class SearchShowItem extends Component {
 	render() {
-		const { show } = this.props;
+		const { show: { id, poster_path, name, first_air_date } } = this.props;
 
-		const source = show.poster_path ?
-			`http://image.tmdb.org/t/p/w154${show.poster_path}` :
+		const source = poster_path ?
+			`http://image.tmdb.org/t/p/w154${poster_path}` :
 			NoPoster;
 
 		return (
-			<Link to={`/tv/${show.id}`}>
+			<Link to={`/tv/${id}`}>
 				<Image src={source} />
-				<h4>{show.name}</h4>
-				<p>{show.first_air_date}</p>
+				<h4>{name}</h4>
+				<p>{moment(first_air_date).format('YYYY')}</p>
 			</Link>
 		);
 	}
