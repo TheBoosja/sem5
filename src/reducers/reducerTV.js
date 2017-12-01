af0942a4a1bb8d7bb979aae64d07963aabb4163c
unknown <mathias.iversen487@gmail.com>
2017-12-01 16:37:52 +0100

    GET_EPISODE

--- src/reducers/reducerTV.js
+++ src/reducers/reducerTV.js
@@ -1,20 +1,26 @@
 import { 
 	GET_SHOW,
 	GET_SEASON,
+	GET_EPISODE,
 	EMPTY_DATA 
 } from '../actions/types';
 
-export default (state = {}, action) => {
-	switch (action.type) {
+export default (state = {}, { type, payload }) => {
+	switch (type) {
 		case GET_SHOW:
 			return {
 				...state,
-				show: action.payload
+				show: payload
 			};
 		case GET_SEASON:
 			return {
 				...state,
-				season: action.payload
+				season: payload
+			};
+		case GET_EPISODE:
+			return {
+				...state,
+				episode: payload
 			};
 		case EMPTY_DATA:
 			return {
