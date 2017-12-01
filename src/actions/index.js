276fa3580a9bdb5ac05d0381ed0c7b2c3d775aca
unknown <mathias.iversen487@gmail.com>
2017-12-01 16:38:27 +0100

    Refactor (get)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,44 @@
 import axios from 'axios';
-import { 
-	GET_SHOW, 
+import {
+	GET_SHOW,
 	GET_SEASON,
-	EMPTY_DATA 
+	GET_EPISODE,
+	EMPTY_DATA
 } from './types';
 
 
 export function getShow(id) {
-	return (dispatch) => {
-		const url = `https://api.themoviedb.org/3/tv/${id}?api_key=e88fbcbf7600bbb2fbc4469a5d75ca09&language=en-US`;
-
-		axios(url).then(res => {
-			dispatch({
-				type: GET_SHOW,
-				payload: res.data
-			});
-		}).catch(error => {
-			console.log(error);
-		});
-	};
+	return get({
+		url: `https://api.themoviedb.org/3/tv/${id}`,
+		type: GET_SHOW
+	});
 }
 
 export function getSeason(tvId, seasonNum) {
+	return get({
+		url: `https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNum}`, 
+		type: GET_SEASON
+	});
+}
+
+export function getEpisode({ tvId, seasonNum, episodeNum }) {
+	return get({
+		url: `https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNum}/episode/${episodeNum}`,
+		type: GET_EPISODE
+	});
+}
+
+function get({ url, type }) {
 	return (dispatch) => {
-		const url = `https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNum}?api_key=e88fbcbf7600bbb2fbc4469a5d75ca09&language=en-US`;
-		axios(url).then(res => {
+		url += '?api_key=e88fbcbf7600bbb2fbc4469a5d75ca09&language=en-US';
+
+		axios(url).then(({ data }) => {
 			dispatch({
-				type: GET_SEASON,
-				payload: res.data
+				type,
+				payload: data
 			});
+		}).catch(error => {
+			console.log(error);
 		});
 	};
 }
