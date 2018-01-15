8c734016163e62c229f98e951e5c813b98835426
unknown <mathias.iversen487@gmail.com>
2018-01-15 11:35:23 +0100

    logMeta beta

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,24 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!");
+});
+
+exports.logMeta = functions.firestore
+	.document('users/{userId}/logs/{logId}')
+	.onWrite(event => {
+		// ... code here
+		var logTarget = event.data.data();
+
+		var metalog = {
+			id: logTarget.id
+		};
+		if (logTarget.season) metalog.season = logTarget.season;
+		if (logTarget.episode) metalog.episode = logTarget.episode;
+
+		console.log(metalog);
+
+		return 0;
+	});
